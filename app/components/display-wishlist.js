4a0daecf725dd24e968bb5fbcbf0610aec4580d7
Ayana Powell and Caleb Paul <ayana+caleb@>
2016-11-02 14:57:47 -0700

    add remove function to wishlist service

---
+++ app/components/display-wishlist.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  wishlist: Ember.inject.service(),
+
+  actions: {
+    removeFromWishlist(item) {
+      console.log('remove: '+item);
+      this.get('wishlist').remove(item);
+    }
+  }
+});
--- app/components/project-tile.js
+++ app/components/project-tile.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  wishlist: Ember.inject.service(),
+
+  actions: {
+    addToWishlist(item) {
+      this.get('wishlist').add(item);
+    },
+
+    // removeFromWishlist(item) {
+    //   console.log('remove: '+item);
+    //   this.get('wishlist').remove(item);
+    // }
+  }
 });
