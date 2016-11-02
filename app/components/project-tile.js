f6b49f7c5f16f5bad817ad48943f368108cbffc8
Ayana Powell and Caleb Paul <ayana+caleb@>
2016-11-02 16:35:31 -0700

    add computed property to show percentage

--- app/components/project-tile.js
+++ app/components/project-tile.js
@@ -3,6 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   wishlist: Ember.inject.service(),
 
+  percentage: Ember.computed('project.current_amount', 'project.goal_amount', function() {
+    var per = (this.get('project.current_amount') / this.get('project.goal_amount')) * 100;
+    return per.toFixed(2);
+  }),
+
   actions: {
     addToWishlist(item) {
       this.get('wishlist').add(item); //adds to wishlist
